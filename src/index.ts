49d6aeefad7e5d89a5d89c6c1f572ad1cf62c507
PrimalDjinn <archiethebig@gmail.com>
2026-07-25 19:29:57 +0300

    chore: update repo

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,57 @@
 import { Container, type StopParams } from "@cloudflare/containers";
+import type { OneOf } from "@chiballc/types";
+
+export interface D1EnvVariables {
+  BAO_D1_ACCOUNT_ID: string;
+  BAO_D1_DATABASE_ID: string;
+  BAO_D1_TOKEN: string;
+}
+
+export interface D1EnvUrl {
+  BAO_D1_CONNECTION_URL: string;
+}
+
+function ensureD1Variables<T extends OneOf<[D1EnvUrl, D1EnvVariables]>>(
+  env: Record<string, any>,
+): asserts env is T {
+  if (typeof env != "object") {
+    throw new Error("Passed env is not an object", { cause: env });
+  }
+
+  if (env.BAO_D1_ACCOUNT_ID && env.BAO_D1_DATABASE_ID && env.BAO_D1_TOKEN) {
+    return;
+  }
+
+  // Format: https://<AccountID>:<APIToken>@<DatabaseID>
+  if (env.BAO_D1_CONNECTION_URL) {
+    return;
+  }
+
+  throw new Error("We could not find the variables to access openbao", {
+    cause: env,
+  });
+}
 
 export class BaoContainer extends Container {
   defaultPort = 8200; // Port the container is listening on
   sleepAfter = "4m"; // Stop the instance if requests not sent for 10 minutes
 
   constructor(ctx: DurableObjectState<{}>, env: Cloudflare.Env) {
-    if(!env.BAO_STATIC_SEAL_KEY) {
-      throw new Error("You need to set a BAO_STATIC_SEAL_KEY environment variable")
+    if (!env.BAO_STATIC_SEAL_KEY) {
+      throw new Error(
+        "You need to set a BAO_STATIC_SEAL_KEY environment variable",
+      );
     }
 
-    // Construct the connection URL required by the OpenBao D1 storage driver:
-    // Format: https://<AccountID>:<APIToken>@<DatabaseID>
-    if(!env.BAO_D1_CONNECTION_URL) {
-      throw new Error("You need to set a BAO_D1_CONNECTION_URL environment variable")
-    }
+    ensureD1Variables(env);
 
     super(ctx, env);
     this.envVars = {
-      BAO_D1_CONNECTION_URL: env.BAO_D1_CONNECTION_URL,
+      BAO_D1_CONNECTION_URL: env.BAO_D1_CONNECTION_URL || "",
       BAO_STATIC_SEAL_KEY: env.BAO_STATIC_SEAL_KEY, // The driver uses this for static unsealing
+      BAO_D1_ACCOUNT_ID: env.BAO_D1_ACCOUNT_ID || "",
+      BAO_D1_DATABASE_ID: env.BAO_D1_DATABASE_ID || "",
+      BAO_D1_TOKEN: env.BAO_D1_TOKEN || "",
       BAO_ADDR: "http://0.0.0.0:8200",
       SKIP_SETCAP: "true",
     };
